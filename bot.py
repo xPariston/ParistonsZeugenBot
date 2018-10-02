258823dd36dea9678499220a1307cc58c31f90f3
Patrick <Patrickwiechert94@gmail.com>
2018-10-02 13:57:30 +0200

    x

--- bot.py
+++ bot.py
@@ -31,8 +31,8 @@ async def Vote(context):
     autor= context.message.author.mention
     output= "Neuer Gesetzesvorschlag \n" + autor + " möchte folgendes zur Wahl stellen:\n" + msg + "\nDie Wahl geht bis " + time
     newmsg_id = await client.say(output)
-    await client.add_reaction(newmsg_id,emoji=":thumbsup:")
-
+    await client.add_reaction(newmsg_id,emoji='👍')
+    await client.add_reaction(newmsg_id,emoji='👎')
 
 @client.command(name='Jukebox',
                 description="Best of Pariston Songs",
