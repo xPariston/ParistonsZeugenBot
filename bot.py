e7fec2d471fa934f7e678467506dae933c5f0e04
Patrick <Patrickwiechert94@gmail.com>
2018-10-04 13:02:27 +0200

    x

--- bot.py
+++ bot.py
@@ -31,7 +31,7 @@ async def WarAnalyse(context):
     parteiliste= []
     async for m in client.logs_from(parteienchannel, 100):
         parteiliste.append(m.content)
-    await client.say(parteiliste + context)
+    await client.say(parteiliste + context.message.content)
 
 
 
