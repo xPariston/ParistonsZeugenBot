459da24b396fc1cfeb466a50de7e328f0fd54a0b
Patrick <Patrickwiechert94@gmail.com>
2018-10-04 15:03:32 +0200

    x

--- bot.py
+++ bot.py
@@ -37,9 +37,9 @@ async def WarAnalyse(context):
 
     GesamtDamage,partydictRawDmg,partydictPerDmg = rrDamage.RefineDamage(warurl,parteiliste)
 
-    Msg1= "Gesamtschaden des Staatenbundes: " + rrDamage.MakeNumber2PrettyString(GesamtDamage) + "\n"
+    Msg1= "Gesamtschaden des Staatenbundes: " + rrDamage.MakeNumber2PrettyString(GesamtDamage) + "\n\n"
     Msg2= "Roher Schaden der Parteien:\n"
-    Msg3= "Prozentualer Schaden der Parteien:\n"
+    Msg3= "\nProzentualer Schaden der Parteien:\n"
     for j in partydictRawDmg:
         Msg2 += j + ":" + rrDamage.MakeNumber2PrettyString(partydictRawDmg[j])+ '\n'
     for i in partydictPerDmg:
