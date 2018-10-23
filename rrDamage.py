c7bc2a8db429a5fab9efb9bdd3bdf41e340cad7d
Patrick <Patrickwiechert94@gmail.com>
2018-10-23 16:59:50 +0200

    x

--- rrDamage.py
+++ rrDamage.py
@@ -192,9 +192,9 @@ async def KriegsAnalyse(url):
         Dmg1h = Differenz[-1]-Differenz[-61]
         Dmg30min= Differenz[-1]-Differenz[-31]
         Dmg10min= Differenz[-1]-Differenz[-11]
-        Dmg1h ="1h: ",MakeNumber2PrettyString(Dmg1h)
-        Dmg30min = "Halbe Stunde: ", MakeNumber2PrettyString(Dmg30min)
-        Dmg10min = "10 min: ", MakeNumber2PrettyString(Dmg10min)
+        Dmg1h ="1h: " + MakeNumber2PrettyString(Dmg1h)
+        Dmg30min = "Halbe Stunde: " + MakeNumber2PrettyString(Dmg30min)
+        Dmg10min = "10 min: " + MakeNumber2PrettyString(Dmg10min)
 
         Dmg1h = Dmg1h.replace("-.","-")
         Dmg30min=Dmg30min.replace("-.","-")
