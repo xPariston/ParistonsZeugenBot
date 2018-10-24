0276f292d8f959e71aca86f16e9edb4090b43ae8
Patrick <Patrickwiechert94@gmail.com>
2018-10-24 10:33:43 +0200

    x

--- bot.py
+++ bot.py
@@ -744,7 +744,7 @@ async def StateWars21d(context):
         GesamtDamage, partydictRawDmg, partydictPerDmg = await rrDamage.MultiWar(Totalwarurllist, parteiliste)
 
         for x in partydictRawDmg:
-            await client.say(x,": ",rrDamage.MakeNumber2PrettyString(partydictRawDmg[x]),"\n")
+            await client.say(x + ": " + rrDamage.MakeNumber2PrettyString(partydictRawDmg[x]) + "\n")
         Msg1 = "Gesamtschaden des Staatenbundes in eigenen Kriegen(%d) während der letzten %d Tage: "%(TotalWars ,days) + rrDamage.MakeNumber2PrettyString(GesamtDamage) + "\n\n"
         Msg2 = "Roher Schaden der Parteien:\n"
         Msg3 = "\nProzentualer Schaden der Parteien:\n"
